e318ea16949efd04aa7551bffbcc994ed58ed5cd
karagara <icey.tea@live.com>
2015-02-16 20:29:48 -0330

    Fix pull request issues

--- src/Server/ServerEntry.java
+++ src/Server/ServerEntry.java
@@ -1,28 +1,27 @@
-package Server;
-
 import static spark.Spark.get;
 import static spark.Spark.post;
 import static spark.Spark.externalStaticFileLocation;
-import spark.Session;
-
-import Server.AccountManager;
+import spark.
 
 public class ServerEntry {
+	private static final String ACCRPAGE = 
+		"<html>"+
+		"<body>"+
+		"<form action ='/form' method='POST'>"+
+		"UserName <input type='text' name='name'><br>"+
+		"Password <input type='password' name='password'><br>"+
+		"Confirm Passowrd <input type='password' name='passwordCon'><br>"+
+		"You have read and agree with terms of use"+
+		"Yes <input type='radio' name='terms' value='yes'><br>"+
+		"No <input type='radio' name='terms' value='no'><br>"+
+		"</form>"+
+		"</body>"+
+		"<html>";
 
-    public static void main(String[] args) {
-        //setup objects
-        AccountManager acctMgmt = new AccountManager();
-
-        //link to static docs
-        externalStaticFileLocation("static");
-
-        //route requests
-        get("/account", (request, response) -> {
-            return acctMgmt.getResponse(request, response);
-        });
-
-        post("/account", (request, response) -> {
-            return acctMgmt.postResponse(request, response);
-        });
-    }
+	 public static void main(String[] args) {
+		externalStaticFileLocation("static");
+		
+		get("/accrPage", (request, response) -> /accrPage.html);
+	}
 }
+
